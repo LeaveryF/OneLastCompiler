a6b29136b4320570e16f9e891c7988fa3526fa96
fpy <fupeiyi@foxmail.com>
2024-06-28 21:26:32 +0800

    feat: add a cout line

--- src/Driver.cpp
+++ src/Driver.cpp
@@ -1,7 +1,9 @@
+#include <iostream>
 #include <olc/frontend/Parser.h>
 
 int main(int argc, char **argv) {
   // TODO: compiler driver
+  std::cout << "test" << '\n';
   olc::parse();
 
   return 0;
